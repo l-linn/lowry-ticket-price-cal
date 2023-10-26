913e3bb27320e1ba70f07729ac8bee5b0e8a7e9e
lin <shilinxi@proton.me>
2023-10-26 12:16:37 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const venue = document.querySelector('#venue');
+console.log(venue);
+
+console.log(venue.id);
+
+const lyricVenue = document.getElementById('venue-lyric');
+console.log(lyricVenue);
+
+const venueOption = document.querySelector('.venue-option');
+console.log(venueOption);
+console.log(venueOption.value)
+
+venueOption.value == 1 ? console.log(venueOption.value): null;
